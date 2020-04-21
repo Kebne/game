9d7d5e10ff5fa164a90807f1e77d06fcbb593ef9
Daniel Torelm <daniel.torelm@kebne.se>
2020-04-21 12:58:10 +0200

    reworked code structure.

--- frontend/modules/gameboard.js
+++ frontend/modules/gameboard.js
@@ -1,19 +1,24 @@
-export function createGameBoard(seats) {
+export function createGameBoard(players) {
 
 class gameBoard extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({mode: 'open'});
     
-    function showSeating(seats, playerNumber = 1) {
-      seats.forEach((element) => {
-        let name = `${element}`;
-        shadow.querySelector(`.player${playerNumber}`).innerHTML = name;
-        shadow.querySelector(`.player${playerNumber}`).style.border = '2px solid pink';
+    function showSeating(playerNumber = 1) {
+      let data = Object.values(players);
+      data.forEach((element) => {
+        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('afterbegin', `${element.name}<br>`);
+        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('beforeend', `Marker: ${element.marks}`);
         playerNumber++;
       });
-      console.log('all players seated');
+      console.log('Alla spelare sitter');
     };
+
+    function pool(pot = 100) {
+      shadow.querySelector('.board').insertAdjacentHTML('afterbegin', `Potten är: ${pot}`);
+    };
+    
     
     
     let templateStyle = document.createElement('style');
@@ -21,39 +26,71 @@ class gameBoard extends HTMLElement {
       :host {display: block;} 
       .game-board {
         display: grid;
-        grid-template-columns: 150px 400px 150px;
+        grid-template-columns: 1fr 2fr 1fr;
         grid-template-rows: 150px 150px 150px;
         grid-template-areas: 
         ". dealer ."
         "player1 board player4"
         "player2 board player3";
-        margin-bottom: 1rem;
+        margin-bottom: 3rem;
+        color: #eee;
       }
-      .dealer{grid-area: dealer; background-color: pink;}
-      .board{grid-area: board; background-color: green;}
-      .player1{grid-area: player1; background-color: blue; box-sizing: border-box;}
-      .player2{grid-area: player2; background-color: black; box-sizing: border-box;}
-      .player3{grid-area: player3; background-color: yellow; box-sizing: border-box;}
-      .player4{grid-area: player4; background-color: grey; box-sizing: border-box;}
+      .dealer {
+        grid-area: dealer; 
+        background-color: #7f384b;
+        padding: 1rem;
+      }
+      .board {
+        grid-area: board; 
+        background-color: #3e7f38;
+        padding: 1rem;
+      }
+      .player1 {
+        grid-area: player1; 
+        background-color: #153244; 
+        box-sizing: border-box; 
+        padding: 1rem;
+      }
+      .player2 {
+        grid-area: player2; 
+        background-color: #4D4940; 
+        box-sizing: border-box;
+        padding: 1rem;
+      }
+      .player3 {
+        grid-area: player3; 
+        background-color: #805238; 
+        box-sizing: border-box;
+        padding: 1rem;
+      }
+      .player4 {
+        grid-area: player4; 
+        background-color: #94855A; 
+        box-sizing: border-box;
+        padding: 1rem;
+      }
+      
     `;
 
     let templateElement = document.createElement('template');
     templateElement.innerHTML = `
       <div class="game-board">
-        <div class="dealer">dealer</div>
+        <div class="dealer">Dealer</div>
 
-        <div class="player1 player">p1</div>
-        <div class="board">board</div>
-        <div class="player4 player">p4</div>
+        <div class="player1 player">
+        </div>
+        <div class="board"></div>
+        <div class="player4 player"></div>
 
-        <div class="player2 player">p2</div>
-        <div class="player3 player">p3</div>
+        <div class="player2 player"></div>
+        <div class="player3 player"></div>
       </div>
     `;
     shadow.append(templateStyle);
     shadow.appendChild(templateElement.content.cloneNode(true));
     
-    showSeating(seats);
+    pool();
+    showSeating();
   }
 };
 window.customElements.define('game-board', gameBoard);
