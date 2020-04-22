6b42f184a3a573f6abd73f347e8531fb6b5b3893
Daniel Torelm <daniel.torelm@kebne.se>
2020-04-22 10:13:30 +0200

    refactored code and alter layout some

--- frontend/modules/gameboard.js
+++ frontend/modules/gameboard.js
@@ -1,23 +1,26 @@
-export function createGameBoard(players) {
+export function createGameBoard(players, gamePot) {
 
 class gameBoard extends HTMLElement {
   constructor() {
     super();
     const shadow = this.attachShadow({mode: 'open'});
     
+    
     function showSeating(playerNumber = 1) {
       let data = Object.values(players);
+      //this need to be updated
+      shadow.querySelector('.board').insertAdjacentHTML('afterbegin', `Potten är: ${gamePot}`);
       data.forEach((element) => {
-        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('afterbegin', `${element.name}<br>`);
-        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('beforeend', `Marker: ${element.marks}`);
+        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('afterbegin', `Namn: ${element.name}<br>`);
+        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('beforeend', `Marker: ${element.marks}<br>`);
+        shadow.querySelector(`.player${playerNumber}`).insertAdjacentHTML('beforeend', `Stolnr: ${element.seat}`);
+        
         playerNumber++;
       });
       console.log('Alla spelare sitter');
     };
 
-    function pool(pot = 100) {
-      shadow.querySelector('.board').insertAdjacentHTML('afterbegin', `Potten är: ${pot}`);
-    };
+   
     
     
     
@@ -27,18 +30,19 @@ class gameBoard extends HTMLElement {
       .game-board {
         display: grid;
         grid-template-columns: 1fr 2fr 1fr;
-        grid-template-rows: 150px 150px 150px;
+        grid-template-rows: 150px 150px;
         grid-template-areas: 
-        ". dealer ."
+       
         "player1 board player4"
         "player2 board player3";
-        margin-bottom: 3rem;
+        margin-bottom: 1rem;
         color: #eee;
       }
       .dealer {
         grid-area: dealer; 
         background-color: #7f384b;
         padding: 1rem;
+        display: none;
       }
       .board {
         grid-area: board; 
@@ -78,18 +82,25 @@ class gameBoard extends HTMLElement {
         <div class="dealer">Dealer</div>
 
         <div class="player1 player">
+          <progress value="900" max="1000"></progress>
         </div>
         <div class="board"></div>
-        <div class="player4 player"></div>
+        <div class="player4 player">
+          <progress value="900" max="1000">70 %</progress>
+        </div>
 
-        <div class="player2 player"></div>
-        <div class="player3 player"></div>
+        <div class="player2 player">
+          <progress value="900" max="1000">70 %</progress>
+        </div>
+        <div class="player3 player">
+          <progress value="900" max="1000">70 %</progress>
+        </div>
       </div>
     `;
     shadow.append(templateStyle);
     shadow.appendChild(templateElement.content.cloneNode(true));
     
-    pool();
+   
     showSeating();
   }
 };
--- frontend/modules/seatplayer.js
+++
@@ -1,3 +0,0 @@
-export function seatPlayer(seats, playerName) {
- seats.push(playerName);
-};
