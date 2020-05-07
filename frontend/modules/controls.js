d5230a561b156f2961f0f265cda4a8138fd8a759
Daniel Torelm <daniel.torelm@kebne.se>
2020-05-07 13:52:41 +0200

    Player controls now moved to its own file

---
+++ frontend/modules/controls.js
@@ -0,0 +1,38 @@
+export class controls extends HTMLElement {
+  
+  constructor() {
+    super();
+   
+    let templateElement = document.createElement('template');
+    templateElement.innerHTML = `
+    <div>
+      <div class="player-marks"></div>
+      <div class="player-status"></div>
+      
+      <div class="game-slider">
+        <label for="playerslider">Att satsa:</label>
+        <input id="playerslider" type="range" min="10" max="100" value="10" step="10" class="bet-slider">
+        <output class="bet-slider-output"></output>
+      </div>
+      <div class="player-input">  
+          <div><button class="js-player-bet player-input-button">Betta</button></div>
+          <div><button class="js-player-pass player-input-button">Passa</button></div>
+          <div><button class="js-player-fold player-input-button">Lägga sig</button></div>
+      </div>
+    </div> 
+    
+      <div>
+        <div class="player-switch">
+          <i>Detta är endast till för testning, detta är inte en del av spelet</i><br>
+          <label for="player-switch-select">Välj spelare:</label>
+          <select class="player-switch-select">
+            <!--options in here dynamical -->
+          </select>
+        </div>
+      </div>
+    `;
+    this.appendChild(templateElement.content.cloneNode(true));
+  };
+
+};
+window.customElements.define('player-controls', controls);
